2a60c1aa9b7a3b179c19e04419673091a9833c6b
TinyInnovator <69229955+TinyInnovator@users.noreply.github.com>
2020-08-05 09:15:13 +0530

    Update inception.py

--- inception.py
+++ inception.py
@@ -1,4 +1,6 @@
 import time
+# use 'pip install Pillow' in cmd to install PIL
+# visit https://pypi.org/project/Pillow/ for more details
 from PIL import ImageGrab
 print("Starting in 3 seconds...")
 time.sleep(3)
